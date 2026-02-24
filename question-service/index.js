918c35e1da539d626941772170f0ccdfebb6cf1e
Harun <theayyyron@gmail.com>
2026-02-24 14:08:28 +0800

    Connect to question router

--- question-service/index.js
+++ question-service/index.js
@@ -16,11 +16,16 @@ app.options(
 )
 app.use(cors());
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}...`)
 })
+
+app.use('/api/questions', require('./routes/questionRoutes'))
 
 app.get('/', (req, res) => {
     res.json({ message: 'Hello World' })
