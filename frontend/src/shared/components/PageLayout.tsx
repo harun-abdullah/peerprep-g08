07403696ca0e9698f03cac77cced061cdca3988e
kohkakohla <ivanjerrickkoh@gmail.com>
2026-03-17 20:23:48 +0800

    Fix broken imports with Hero UI config

--- frontend/src/shared/components/PageLayout.tsx
+++ frontend/src/shared/components/PageLayout.tsx
@@ -6,7 +6,7 @@ interface PageLayoutProps {
 
 export default function PageLayout({ children }: PageLayoutProps) {
   return (
-    <div className="flex flex-col h-screen bg-gray-50 w-screen">
+    <div className="flex flex-col h-screen w-screen">
       <AppNavbar />
       {children}
     </div>
