03e924f259ce5fcabe0455b2478f57d78dcfbef5
Harun <theayyyron@gmail.com>
2026-04-02 11:26:33 +0800

    Create initial MatchingPage for matching service

---
+++ frontend/src/features/matching/pages/MatchingPage.tsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import PageLayout from "../../../shared/components/PageLayout";
+
+export class MatchingPage extends Component {
+  render() {
+    return (
+      <PageLayout>
+        <h1>Matching Page</h1>
+      </PageLayout>
+    );
+  }
+}
+
+export default MatchingPage;
